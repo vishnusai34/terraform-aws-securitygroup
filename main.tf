b7eaf63b7895389d477795c2d268f59e8d192ffe
vishnusai34 <vishnu.karimisetty@gmail.com>
2025-08-21 18:11:10 +0530

    added

--- main.tf
+++ main.tf
@@ -1,7 +1,16 @@
 resource "aws_security_group" "main" {
   name        = var.sg_name
   description = var.sg_description
-  vpc_id      = var.vpc_id
+  vpc_id      = var.
+  
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 
   tags = merge(
     var.sg_tags,
